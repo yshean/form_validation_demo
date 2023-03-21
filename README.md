0dbeeb7111b2ba4fa2f044424efbcb9bc55f68f4
Yong Shean <yong.shean.chong@comerge.net>
2023-03-21 16:30:02 +0800

    init with flutter create

---
+++ README.md
@@ -0,0 +1,3 @@
+# form_validation_simple
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/form_validation_simple/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_validation_simple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
